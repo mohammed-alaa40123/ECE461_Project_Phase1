776659b96c93b73d3d80c26401d7f99e31a6a03b
mohammed-alaa40123 <mohammedalaa40123@gmail.com>
2024-09-08 20:02:17 -0400

    logger working and fixed bug

--- dist/api.js
+++ dist/api.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { Octokit } from "@octokit/core";
 import * as dotenv from "dotenv";
 import axios from 'axios';
@@ -5,53 +14,55 @@ const NPM_REGISTRY_URL = 'https://registry.npmjs.org/-/v1/search?text=$';
 dotenv.config();
 const env = process.env;
 export class API {
-    package_name;
     constructor(name) {
         this.package_name = name;
     }
 }
 export class Git_Hub extends API {
-    owner_name;
     constructor(p_name, own_name) {
         super(p_name);
         this.owner_name = own_name;
     }
-    async getData() {
-        const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
-        try {
-            const response = await octokit.request("GET /repos/{owner}/{repo}", {
-                owner: this.owner_name,
-                repo: this.package_name,
-                headers: {
-                    "X-GitHub-Api-Version": "2022-11-28",
-                },
-            });
-            console.log("Package info fetched successfully");
-            return response.data;
-        }
-        catch (error) {
-            console.error("Error fetching package info:", error);
-            throw error;
-        }
+    getData() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
+            try {
+                const response = yield octokit.request("GET /repos/{owner}/{repo}", {
+                    owner: this.owner_name,
+                    repo: this.package_name,
+                    headers: {
+                        "X-GitHub-Api-Version": "2022-11-28",
+                    },
+                });
+                console.log("Package info fetched successfully");
+                return response.data;
+            }
+            catch (error) {
+                console.error("Error fetching package info:", error);
+                throw error;
+            }
+        });
     }
 }
 export class NPM extends API {
     constructor(p_name) {
         super(p_name);
     }
-    async getData() {
-        try {
-            const response = await axios.get(`${NPM_REGISTRY_URL}/${this.package_name}`);
-            console.log("Package info fetched successfully");
-            return response.data;
-        }
-        catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error(`Error fetching package info: ${error.message}`);
+    getData() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const response = yield axios.get(`${NPM_REGISTRY_URL}/${this.package_name}`);
+                console.log("Package info fetched successfully");
+                return response.data;
             }
-            else {
-                console.error('Unexpected error:', error);
+            catch (error) {
+                if (axios.isAxiosError(error)) {
+                    console.error(`Error fetching package info: ${error.message}`);
+                }
+                else {
+                    console.error('Unexpected error:', error);
+                }
             }
-        }
+        });
     }
 }
--- dist/commands/URLFileCommand.js
+++ dist/commands/URLFileCommand.js
@@ -1,42 +1,59 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import * as fs from 'fs';
 import { Git_Hub, NPM } from '../api.js';
 export class URLFileCommand {
-    static async run(file) {
-        console.log(`Processing URL file: ${file}`);
-        fs.readFile(file, 'utf8', async (err, data) => {
-            if (err) {
-                console.error(`Error reading ${file}:`, err);
-                process.exit(1);
-                return;
-            }
-            const urls = data.split('\n').filter(url => url.trim() !== '');
-            for (const url of urls) {
-                if (url.includes('github.com')) {
-                    console.log(`GitHub package: ${url}`);
-                    const [owner, repo] = url.split('github.com/')[1].split('/');
-                    const githubRepo = new Git_Hub(repo, owner);
-                    const data = await githubRepo.getData();
-                    console.log("GitHub Data:", data);
+    static run(file) {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log(`Processing URL file: ${file}`);
+            fs.readFile(file, 'utf8', (err, data) => __awaiter(this, void 0, void 0, function* () {
+                if (err) {
+                    console.error(`Error reading ${file}:`, err);
+                    process.exit(1);
+                    return;
                 }
-                else if (url.includes('npmjs.com')) {
-                    console.log(`npm package: ${url}`);
-                    const packageName = url.split('package/')[1];
-                    const npmPackage = new NPM(packageName);
-                    const data = await npmPackage.getData();
-                    console.log("NPM Data:");
-                    const npmData = data;
-                    npmData.objects.forEach((obj) => {
-                        console.log(`Package Name: ${obj.package.name}`);
-                        console.log(`Version: ${obj.package.version}`);
-                        console.log(`Description: ${obj.package.description}`);
-                        console.log(`Score: ${obj.score.final}`);
-                        console.log('-----------------------------');
-                    });
+                const urls = data.split('\n').map(url => url.trim()).filter(url => url !== '');
+                for (let url of urls) {
+                    if (url.includes('github.com')) {
+                        if (url.includes('%0D')) {
+                            url = url.replace('%0D', '');
+                        }
+                        if (url.includes('\r')) {
+                            url = url.replace('\r', '');
+                        }
+                        console.log(`GitHub package: ${url}`);
+                        const [owner, repo] = url.split('github.com/')[1].split('/');
+                        const githubRepo = new Git_Hub(repo, owner);
+                        const data = yield githubRepo.getData();
+                        console.log("GitHub Data:", data);
+                    }
+                    else if (url.includes('npmjs.com')) {
+                        console.log(`npm package: ${url}`);
+                        const packageName = url.split('package/')[1];
+                        const npmPackage = new NPM(packageName);
+                        const data = yield npmPackage.getData();
+                        console.log("NPM Data:");
+                        const npmData = data;
+                        npmData.objects.forEach((obj) => {
+                            console.log(`Package Name: ${obj.package.name}`);
+                            console.log(`Version: ${obj.package.version}`);
+                            console.log(`Description: ${obj.package.description}`);
+                            console.log(`Score: ${obj.score.final}`);
+                            console.log('-----------------------------');
+                        });
+                    }
+                    else {
+                        console.log(`Unknown package source: ${url}`);
+                    }
                 }
-                else {
-                    console.log(`Unknown package source: ${url}`);
-                }
-            }
+            }));
         });
     }
 }
