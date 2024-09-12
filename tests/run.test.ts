e801fac563e93c3ec29a45421951dbdbee602379
andrewcali <acali@purdue.edu>
2024-09-12 15:44:13 -0400

    testing output of install

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -2,47 +2,52 @@ import * as fs from 'fs';
 import { exec } from 'child_process';
 import { InstallCommand } from '../src/commands/InstallCommand.ts';
 
-jest.mock('child_process', () => ({
-  exec: jest.fn(),
-}));
+// jest.mock('child_process', () => ({
+//   exec: jest.fn(),
+// }));
 
-jest.mock('fs', () => ({
-  existsSync: jest.fn(),
-  mkdirSync: jest.fn(),
-  unlinkSync: jest.fn(),
-  stat: jest.fn((path, callback) => callback(null, { isFile: () => true })),
-  createWriteStream: jest.fn(() => ({
-    write: jest.fn(),
-    end: jest.fn(),
-  })),
-}));
+// jest.mock('fs', () => ({
+//   existsSync: jest.fn(),
+//   mkdirSync: jest.fn(),
+//   unlinkSync: jest.fn(),
+//   stat: jest.fn((path, callback) => callback(null, { isFile: () => true })),
+//   createWriteStream: jest.fn(() => ({
+//     write: jest.fn(),
+//     end: jest.fn(),
+//   })),
+// }));
 
 describe('TestCommand', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  // afterEach(() => {
+  //   jest.clearAllMocks();
+  // });
 
   test('should install a dependency', (done) => {
     const dependency = 'lodash';
-    const execMock = exec as unknown as jest.Mock;
-    execMock.mockImplementation((command, callback) => {
-      if (command.includes(dependency)) {
-        callback(null, 'installed', '');
-      } else {
-        callback(new Error('Failed to install'), '', '');
-      }
-    });
+    // const execMock = exec as unknown as jest.Mock;
+    // execMock.mockImplementation((command, callback) => {
+    //   if (command.includes(dependency)) {
+    //     callback(null, 'installed', '');
+    //   } else {
+    //     callback(new Error('Failed to install'), '', '');
+    //   }
+    // });
 
-    const fsMock = fs.existsSync as jest.Mock;
-    fsMock.mockImplementation((path) => {
-      return path.includes(`node_modules/${dependency}`);
-    });
+    // const fsMock = fs.existsSync as jest.Mock;
+    // fsMock.mockImplementation((path) => {
+    //   return path.includes(`node_modules/${dependency}`);
+    // });
+
+    // Capture console output
+    const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
 
     // Run the installDependency method
     InstallCommand.installDependency(dependency)
       .then(() => {
         // Verify that the dependency was installed
-        expect(fs.existsSync(`node_modules/${dependency}`)).toBe(true);
+        //expect(fs.existsSync(`node_modules/${dependency}`)).toBe(true);
+        expect(consoleLogSpy).toHaveBeenCalledWith('Successfully installed lodash');
+        consoleLogSpy.mockRestore();
         done();
       })
       .catch((error) => {
