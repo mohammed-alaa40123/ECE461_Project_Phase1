f085c6612315f88b3f5279156dd4320e7c7eeeb3
mohammed-alaa40123 <mohammedalaa40123@gmail.com>
2024-09-22 21:59:50 -0400

    checker done

--- src/commands/TestCommand.ts
+++ src/commands/TestCommand.ts
@@ -1,23 +1,21 @@
 import { spawn } from 'child_process';
 import * as fs from 'fs';
-import logger from '../logger';
 
 export class TestCommand {
   public static run(): void {
     console.log('Running tests...');
-    logger.info('Running tests...');
-    this.runTests((testError) => {
+    this.runTests((testError, formattedOutput) => {
       if (testError) {
         console.error('Error running tests:', testError);
-        logger.error('Error running tests:', testError);
         process.exit(1); // Exit with a non-zero status code to indicate failure
       } else {
+        console.log(formattedOutput); // Output the formatted result
         process.exit(0); // Exit with a zero status code to indicate success
       }
     });
   }
 
-  private static runTests(callback: (error: string | null) => void): void {
+  private static runTests(callback: (error: string | null, output?: string) => void): void {
     const testProcess = spawn('npx', ['jest', '--coverage', '--config', 'jest.config.cjs']);
 
     const stdoutStream = fs.createWriteStream('test-output.txt');
@@ -56,12 +54,11 @@ export class TestCommand {
           if (testMatch && coverageMatch) {
             const passedTests = parseInt(testMatch[1]);
             const totalTests = parseInt(testMatch[2]);
-            const lineCoverage = parseFloat(coverageMatch[1]);
+            const lineCoverage = parseInt(coverageMatch[1]);
 
             const formattedOutput = `${passedTests}/${totalTests} test cases passed. ${lineCoverage}% line coverage achieved.`;
 
-            console.log(formattedOutput);
-            callback(null);
+            callback(null, formattedOutput);
           } else {
             callback('Error parsing test results or coverage information');
           }
--- src/tests/run.test.ts
+++ src/tests/run.test.ts
@@ -6,22 +6,10 @@ describe("InstallCommand", () => {
 
     beforeAll(async () => {
       result = await InstallCommand.run("userland.txt");
-    }, 10000); // Increase the timeout to 10 seconds
+    }, 100000); // Increase the timeout to 10 seconds
 
     test("should install dependencies successfully", async () => {
       expect(result).toBe(0);
-    }, 10000); // Increase the timeout to 10 seconds
-  });
-
-  describe("Error reading file", () => {
-    let result: number | undefined;
-
-    beforeAll(async () => {
-      result = await InstallCommand.run("userlan1d.txt");
-    }, 10000); // Increase the timeout to 10 seconds
-
-    test("should handle error reading file", async () => {
-      expect(result).toBe(undefined);
     }, 10000); // Increase the timeout to 10 seconds
   });
 });
